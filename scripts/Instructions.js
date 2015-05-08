9425a216d44cb71450ac0f22e1f958589e0ef9cb
AdamSlater <Adam.Slater.1010@gmail.com>
2015-05-08 15:16:47 -0700

    added comments to instructions

--- scripts/Instructions.js
+++ scripts/Instructions.js
@@ -1,6 +1,7 @@
 var texts = ["This is the grid.", "When a square flashes yellow, click that square.", "Click the wrong square and it flashes red."];
 var counter = 0;
 
+/*Displays next image + text instruction*/
 function next() {
     var text = document.getElementById('instruct');
     counter++;
@@ -10,6 +11,7 @@ function next() {
     text.innerHTML = texts[counter];
 }
 
+/*Displays previous image + text instruction*/
 function previous() {
     var text = document.getElementById('instruct');
     counter--;
@@ -19,6 +21,8 @@ function previous() {
     text.innerHTML = texts[counter];
 }
 
+
+/*Fade effect between slides*/
 jQuery("#slideshow").cycle({
 timeout:0,
 fx: 'fade',
